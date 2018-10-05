23f26357a2291fdef02e1d062b60d05d8eb89e87
Helinia <yex@mcmaster.ca>
2018-10-05 15:53:26 -0400

    added blog apps and made migrations

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class Blog(models.Model):
+    #Create a blog models
+    #title
+    title = models.CharField(max_length = 255)
+    #publication date
+    pub_date = models.DateTimeField()
+    #body
+    blog_body = models.TextField()
+    #image
+    image = models.ImageField(upload_to = 'images/')
+
+
+
+#add the blog app to the settings
+#create a migration in terminal
+#migrate
+#add to admin
